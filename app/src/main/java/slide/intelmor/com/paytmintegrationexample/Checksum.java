aa3aed406de212973e6f344cee04c26e4d1c4938
pavan <pavan.intelmor@gmail.com>
2018-08-16 17:57:07 +0530

    paytmintegration

---
+++ app/src/main/java/slide/intelmor/com/paytmintegrationexample/Checksum.java
@@ -0,0 +1,37 @@
+package slide.intelmor.com.paytmintegrationexample;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by HP on 8/13/2018.
+ */
+
+public class Checksum {
+
+    @SerializedName("CHECKSUMHASH")
+    private String checksumHash;
+
+    @SerializedName("ORDER_ID")
+    private String orderId;
+
+    @SerializedName("payt_STATUS")
+    private String paytStatus;
+
+    public Checksum(String checksumHash, String orderId, String paytStatus) {
+        this.checksumHash = checksumHash;
+        this.orderId = orderId;
+        this.paytStatus = paytStatus;
+    }
+
+    public String getChecksumHash() {
+        return checksumHash;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getPaytStatus() {
+        return paytStatus;
+    }
+}
---
+++ app/src/main/java/slide/intelmor/com/paytmintegrationexample/Paytm.java
@@ -0,0 +1,100 @@
+package slide.intelmor.com.paytmintegrationexample;
+
+import android.util.Log;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.UUID;
+
+/**
+ * Created by HP on 8/13/2018.
+ */
+
+public class Paytm {
+
+    @SerializedName("MID")
+    String mId;
+
+    @SerializedName("ORDER_ID")
+    String orderId;
+
+    @SerializedName("CUST_ID")
+    String custId;
+
+    @SerializedName("CHANNEL_ID")
+    String channelId;
+
+    @SerializedName("TXN_AMOUNT")
+    String txnAmount;
+
+    @SerializedName("WEBSITE")
+    String website;
+
+    @SerializedName("CALLBACK_URL")
+    String callBackUrl;
+
+    @SerializedName("INDUSTRY_TYPE_ID")
+    String industryTypeId;
+
+    public Paytm(String mId, String channelId, String txnAmount, String website, String callBackUrl, String industryTypeId) {
+        this.mId = mId;
+        this.orderId = generateString();
+        this.custId = generateString();
+        this.channelId = channelId;
+        this.txnAmount = txnAmount;
+        this.website = website;
+        this.callBackUrl = callBackUrl;
+        this.industryTypeId = industryTypeId;
+
+        Log.d("orderId", orderId);
+        Log.d("customerId", custId);
+        Log.d("mId", mId);
+        Log.d("channelId", channelId);
+        Log.d("txnAmount", txnAmount);
+        Log.d("website", website);
+        Log.d("callBackUrl", callBackUrl+orderId);
+        Log.d("industryTypeId", industryTypeId);
+    }
+
+    public String getmId() {
+        return mId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public String getTxnAmount() {
+        return txnAmount;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getCallBackUrl() {
+        return callBackUrl;
+    }
+
+    public String getIndustryTypeId() {
+        return industryTypeId;
+    }
+
+    /*
+    * The following method we are using to generate a random string everytime
+    * As we need a unique customer id and order id everytime
+    * For real scenario you can implement it with your own application logic
+    * */
+    private String generateString() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid.replaceAll("-", "");
+    }
+}
